585ca2f5afc24cdfc8c35d86a1ca6e441750ebaa
sshindanai <sshindanai.m@gmail.com>
2022-12-31 17:03:22 +0700

    init commit

---
+++ src/main/java/com/sshindanai/Main.java
@@ -0,0 +1,51 @@
+package com.sshindanai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/customers")
+public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerReq(String name, String email, Integer age) {}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerReq newCustomerReq) {
+        Customer customer = new Customer();
+        customer.setName(newCustomerReq.name());
+        customer.setEmail(newCustomerReq.email());
+        customer.setAge(newCustomerReq.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerReq newCustomerReq) {
+        Customer customer = customerRepository.findById(id).orElseThrow();
+        customer.setName(newCustomerReq.name());
+        customer.setEmail(newCustomerReq.email());
+        customer.setAge(newCustomerReq.age());
+        customerRepository.save(customer);
+    }
+}
